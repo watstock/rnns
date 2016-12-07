b1c5060315c073d60e710f7191a30f4fa4fc6f42
alexey-ernest <alexey.ernest@gmail.com>
2016-12-07 20:24:35 +0300

    1-300-1. 15 timesteps, 60 valset

--- watstock.py
+++ watstock.py
@@ -136,9 +136,9 @@ def main():
   symbol = 'AAPL'
   date_from = '2006-12-05'
   date_to = '2016-12-05'
-  tsteps = 5
+  tsteps = 15
   testset = 30
-  layers = [1000]
+  layers = [300]
 
   batch_size = 10
   valset = 60
