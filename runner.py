716f72f368c982f311f05fb3a1905ce9a9ba7add
alexey-ernest <alexey.ernest@gmail.com>
2016-12-15 22:09:45 +0300

    reorder

--- runner.py
+++ runner.py
@@ -129,7 +129,7 @@ def train_symbol(symbol):
 
 def main():
 
-  symbols = reverse(['AMZN', 'FB', 'GOOGL', 'GRPN', 'NFLX', 'NVDA', 'PCLN', 'TSLA', 'AAPL'])
+  symbols = reverse(['AAPL', 'AMZN', 'FB', 'GOOGL', 'GRPN', 'NFLX', 'NVDA', 'PCLN', 'TSLA'])
   for symbol in symbols:
     train_symbol(symbol)
 
