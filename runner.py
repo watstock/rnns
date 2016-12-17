4bb3ef4e1df1f60e2cd71b8077f409c04bc20e03
alexey-ernest <alexey.ernest@gmail.com>
2016-12-17 10:50:09 +0300

    re-run tickers

--- runner.py
+++ runner.py
@@ -121,20 +121,21 @@ def train_symbol(symbol):
   # build param sequence
   params = build_params(
     architectures=(
-      [[50], None], 
-      [[100], None], 
-      [[200], None], 
-      [[300], None], 
+      # [[50], None], 
+      # [[100], None], 
+      # [[200], None], 
+      # [[300], None], 
       [[500], None], 
-      [[1000], None], 
-      [[1500], None], 
-      [[2000], None], 
-      [[100,100], 0.2], 
-      [[100,300], 0.2], 
-      [[300,300], 0.2], 
-      [[100,300,100], 0.2]
+      # [[1000], None], 
+      # [[1500], None], 
+      # [[2000], None], 
+      # [[100,100], 0.2], 
+      # [[100,300], 0.2], 
+      # [[300,300], 0.2], 
+      # [[100,300,100], 0.2]
     ),
-    timesteps=(3, 5, 10, 15, 20, 30, 50, 60, 90)
+    #timesteps=(3, 5, 10, 15, 20, 30, 50, 60, 90)
+    timesteps=(3, 5, 10, 30)
   )
 
   param_sequence = []
@@ -160,7 +161,7 @@ def train_symbol(symbol):
 def main():
 
   # ['AAPL', 'AMZN', 'FB', 'GOOGL', 'GRPN', 'NFLX', 'NVDA', 'PCLN', 'TSLA']
-  symbols = ['NVDA']
+  symbols = ['AAPL', 'AMZN', 'FB', 'GOOGL', 'GRPN', 'NFLX', 'NVDA', 'PCLN', 'TSLA']
   for symbol in symbols:
     train_symbol(symbol)
 
